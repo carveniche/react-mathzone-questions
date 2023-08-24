c0413b31bc7ae659934eccf5d1a3d67061fdedd7
anishtiwari09 <aneeshtiwari24@gmail.com>
2023-08-24 15:44:57 +0530

    commit

--- FillInTheBlacks/MainFillInTheBlanks.jsx
+++ FillInTheBlacks/MainFillInTheBlanks.jsx
@@ -1,10 +1,16 @@
-import React from "react"
+import React from "react";
 import FillInTheBlanks from "./FillInTheBlanks";
 
-export default function MainFillInTheBlanks({obj,meter,choiceId}){
-  let studentAnswer=""
-  let question_data=obj?.question_data||[]
-  question_data=question_data[0]||{}
-  studentAnswer=question_data?.studentAnswer
-    return <FillInTheBlanks state={obj?.question_data[0]} meter={meter} choiceId={studentAnswer}/>
+export default function MainFillInTheBlanks({ obj, meter, choiceId }) {
+  let studentAnswer = "";
+  let question_data = obj?.question_data || [];
+  question_data = question_data[0] || {};
+  studentAnswer = question_data?.studentAnswer;
+  return (
+    <FillInTheBlanks
+      state={obj?.question_data[0]}
+      meter={meter}
+      choiceId={studentAnswer || choiceId}
+    />
+  );
 }
--- MultipleChoice/MainMultipleChoice.jsx
+++ MultipleChoice/MainMultipleChoice.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import MultipleChoice from "./MultipleChoice";
-export default function MainMultipleChoice({obj,meter,choiceId}) {
-  let studentAnswer=""
-  let question_data=obj?.question_data||[]
-  question_data=question_data[0]||{}
-  studentAnswer=question_data?.studentAnswer
-  return <MultipleChoice state={obj?.question_data[0]} meter={meter} choiceId={studentAnswer}/>;
+export default function MainMultipleChoice({ obj, meter, choiceId }) {
+  let studentAnswer = "";
+  let question_data = obj?.question_data || [];
+  question_data = question_data[0] || {};
+  studentAnswer = question_data?.studentAnswer;
+
+  return (
+    <MultipleChoice
+      state={obj?.question_data[0]}
+      meter={meter}
+      choiceId={studentAnswer || choiceId}
+    />
+  );
 }
