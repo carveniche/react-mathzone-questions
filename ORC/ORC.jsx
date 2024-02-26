7475d563f867dc735e24bc9e1acf4424bdcd53b1
anishtiwari09 <aneeshtiwari24@gmail.com>
2024-02-26 10:34:54 +0530

    fixed orc type question

--- ORC/ORC.jsx
+++ ORC/ORC.jsx
@@ -54,6 +54,36 @@ const compareArrayText = (arr1, arr2) => {
   }
   return true;
 };
+const disabledSelectChoice = (arr) => {
+  let parents = document.querySelector("#orc_question_type");
+  let selectBox = parents?.querySelectorAll("select");
+  for (let i = 0; i < selectBox.length; i++) {
+    let options = selectBox[i]?.querySelectorAll("option");
+    for (let j = 0; j < options.length; j++) {
+      let values = options[j]?.value;
+      if (arr[i]?.includes(values)) {
+        options[j].setAttribute("selected", "true");
+      } else {
+        options[j]?.removeAttribute("selected");
+      }
+    }
+  }
+};
+const disabledCkeditor = () => {
+  let parents = document.querySelector("#orc_question_type");
+  let inputBox = parents?.querySelectorAll("select") || [];
+  for (let item of inputBox) {
+    if (item) {
+      item.setAttribute("disabled", true);
+    }
+  }
+  inputBox = parents?.querySelectorAll("input") || [];
+  for (let item of inputBox) {
+    if (item) {
+      item.setAttribute("disabled", true);
+    }
+  }
+};
 function ORC({ obj, question_text, meter }) {
   const [redAlert, setRedAlert] = useState(false);
   let [dragState, setDragState] = useState([]);
@@ -74,6 +104,10 @@ function ORC({ obj, question_text, meter }) {
     } catch (e) {
       console.log(e);
     }
+  }, []);
+  useEffect(() => {
+    disabledSelectChoice();
+    disabledCkeditor();
   }, []);
   useEffect(() => {
     let temp = [];
@@ -297,7 +331,7 @@ function ORC({ obj, question_text, meter }) {
       }
       {redAlert && !hasAnswerSubmitted && <CustomAlertBoxMathZone />}
       <div id="studentAnswerResponse">
-        <div className="mathzoneQuestionName">
+        <div className="mathzoneQuestionName" id="orc_question_type">
           {parse(question_text, optionSelect)}
         </div>
         {obj?.upload_file_name && (
