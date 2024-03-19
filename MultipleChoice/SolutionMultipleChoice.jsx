2d92ced76af7405f34d61da73986492073e9e819
anishtiwari09 <aneeshtiwari24@gmail.com>
2024-03-19 17:46:50 +0530

    fixed latex

--- MultipleChoice/SolutionMultipleChoice.jsx
+++ MultipleChoice/SolutionMultipleChoice.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import HtmlParser from "react-html-parser";
 import styles from "../OnlineQuiz.module.css";
+import parse from "html-react-parser";
+import { optionSelectStaticMathField } from "../HorizontalFillUpsEquationType/replaceDomeNode/ReplaceDomNode";
 function SolutionMultipleChoice({ model, type, hideSolutionText }) {
   return (
     <div
@@ -20,21 +21,29 @@ function SolutionMultipleChoice({ model, type, hideSolutionText }) {
         (item, i) =>
           (item.correct ||
             String(type).trim() == "Fill in the blanks ".trim()) && (
-            <>
-              <div key={i}>{HtmlParser(item.choices)}</div>
-              {item?.solution && <div>{HtmlParser(item?.solution)}</div>}
+            <React.Fragment key={i}>
+              <div key={i}>
+                {parse(item.choices, optionSelectStaticMathField)}
+              </div>
+              {item?.solution && (
+                <div>{parse(item?.solution, optionSelectStaticMathField)}</div>
+              )}
               {item?.solution_image && (
                 <div>{<img src={item?.solution_image} />}</div>
               )}
-              {item?.solution1 && <div>{HtmlParser(item?.solution1)}</div>}
+              {item?.solution1 && (
+                <div>{parse(item?.solution1, optionSelectStaticMathField)}</div>
+              )}
               {item?.solution1_image && (
                 <div>{<img src={item?.solution1_image} />}</div>
               )}
-              {item?.solution2 && <div>{HtmlParser(item?.solution2)}</div>}
+              {item?.solution2 && (
+                <div>{parse(item?.solution2, optionSelectStaticMathField)}</div>
+              )}
               {item?.solution2_image && (
                 <div>{<img src={item?.solution2_image} />}</div>
               )}
-            </>
+            </React.Fragment>
           )
       )}
     </div>
--- TeacherOnlineQuiz/component/AllDisabledQuestion/MultipleChoice/SelectMultipleChoice.jsx
+++ TeacherOnlineQuiz/component/AllDisabledQuestion/MultipleChoice/SelectMultipleChoice.jsx
@@ -1,103 +1,137 @@
-import React, { useEffect } from "react"
-import {useState,useRef} from "react"
-import HtmlParser from "react-html-parser/lib/HtmlParser";
+import React, { useEffect } from "react";
+import { useState, useRef } from "react";
+import parse from "html-react-parser";
 import styles from "../OnlineQuiz.module.css";
 import styled from "styled-components";
 import { useContext } from "react";
 import { ValidationContext } from "../../../../../MainOnlineQuiz/MainOnlineQuizPage";
-export default function SelectMultipleChoice({choices,inputRef,answerHasSelected,resultView,choiceId}){
-    const {isStudentAnswerResponse}=useContext(ValidationContext)
-    let [choicesState,setChoicesState]=useState([])
-    let prev=useRef(0)
-    useEffect(()=>{
-        let arr=[]
-        choices?.map((item)=>{
-            let obj={...item,show:false}
-            arr.push({...obj})
-        })
-        setChoicesState([...arr])
-    },[])
-    const handleChoiceSelection=(i)=>{
-        if(answerHasSelected||isStudentAnswerResponse)
-        return
-        choicesState[prev.current].show=false
-        choicesState[i].show=true
-        setChoicesState([...choicesState])
-        prev.current=i
-    }
-    inputRef.current=choicesState
-    return <FlexBox >
-    {choicesState?.map((value,i)=>(value.choices||value?.choice_image)&&<><div className={`${styles.flex}  ${(isStudentAnswerResponse&&String(choiceId)===String(value?.choice_id)?.trim())?styles.selectedChoiceType:value.show?styles.selectedChoiceType:styles.prevSelectionAnswerSelection} ${value?.correct===true&&resultView===true&&'correctChoicesMultiple'}`} style={{padding:`${value?.choice_image?0.5:1}rem 1rem`,position:'relative'}} key={i}onClick={()=>handleChoiceSelection(i) }>
-   
-        <div> <b>{String.fromCharCode(65 + i)}</b>
-        
-        </div>
-       <div  className={`${styles.flex} ${styles.flexDirectionColumn}` }>
-        {value.choices&&<div key={i} >{HtmlParser(value.choices)} </div>}
-        {value?.choice_image&&<div className="choiceImage"><img src={value?.choice_image} /></div>}
-        
-        </div>
-        {resultView===true&&value?.student_choice===true&&<div className={styles.circleResponse}></div>}
-    </div ></>)}
-        </FlexBox >
+import { optionSelectStaticMathField } from "../../../../HorizontalFillUpsEquationType/replaceDomeNode/ReplaceDomNode";
+export default function SelectMultipleChoice({
+  choices,
+  inputRef,
+  answerHasSelected,
+  resultView,
+  choiceId,
+}) {
+  const { isStudentAnswerResponse } = useContext(ValidationContext);
+  let [choicesState, setChoicesState] = useState([]);
+  let prev = useRef(0);
+  useEffect(() => {
+    let arr = [];
+    choices?.map((item) => {
+      let obj = { ...item, show: false };
+      arr.push({ ...obj });
+    });
+    setChoicesState([...arr]);
+  }, []);
+  const handleChoiceSelection = (i) => {
+    if (answerHasSelected || isStudentAnswerResponse) return;
+    choicesState[prev.current].show = false;
+    choicesState[i].show = true;
+    setChoicesState([...choicesState]);
+    prev.current = i;
+  };
+  inputRef.current = choicesState;
+  return (
+    <FlexBox>
+      {choicesState?.map(
+        (value, i) =>
+          (value.choices || value?.choice_image) && (
+            <React.Fragment key={i}>
+              <div
+                className={`${styles.flex}  ${
+                  isStudentAnswerResponse &&
+                  String(choiceId) === String(value?.choice_id)?.trim()
+                    ? styles.selectedChoiceType
+                    : value.show
+                    ? styles.selectedChoiceType
+                    : styles.prevSelectionAnswerSelection
+                } ${
+                  value?.correct === true &&
+                  resultView === true &&
+                  "correctChoicesMultiple"
+                }`}
+                style={{
+                  padding: `${value?.choice_image ? 0.5 : 1}rem 1rem`,
+                  position: "relative",
+                }}
+                key={i}
+                onClick={() => handleChoiceSelection(i)}
+              >
+                <div>
+                  {" "}
+                  <b>{String.fromCharCode(65 + i)}</b>
+                </div>
+                <div className={`${styles.flex} ${styles.flexDirectionColumn}`}>
+                  {value.choices && (
+                    <div key={i}>
+                      {parse(value.choices, optionSelectStaticMathField)}{" "}
+                    </div>
+                  )}
+                  {value?.choice_image && (
+                    <div className="choiceImage">
+                      <img src={value?.choice_image} />
+                    </div>
+                  )}
+                </div>
+                {resultView === true && value?.student_choice === true && (
+                  <div className={styles.circleResponse}></div>
+                )}
+              </div>
+            </React.Fragment>
+          )
+      )}
+    </FlexBox>
+  );
 }
-const FlexBox=styled.div`
-display:flex;
-flex-wrap:wrap;
-gap:0.5rem;
-width:90%;
-margin-Top: 1.5rem;
-cursor: pointer;
-> .correctChoicesMultiple{
-    background-color:#CCEEA5;
-}
-> div{
-    min-width:Calc(50% - 0.5rem);
-    
-    flex:1;
-    display:flex;
-font-family: Montserrat;
-font-size: 16px;
-font-style: normal;
-font-weight: 600;
-line-height: 24px;
-letter-spacing: 0em;
-text-align: left;
-align-items: center;
-padding-left:18px;
-color: #233584;
-border-radius: 12px;
-word-break:break;
-min-height: auto;
-height: 60px;
+const FlexBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  width: 90%;
+  margin-top: 1.5rem;
+  cursor: pointer;
+  > .correctChoicesMultiple {
+    background-color: #cceea5;
+  }
+  > div {
+    min-width: Calc(50% - 0.5rem);
 
-gap: 2rem;
-
-border: 1px solid black;
-
-height:auto;
-
-
- 
-}
-> div > div{
-    min-width:auto;
-    min-height:auto;
-    
-    
-  
-}
-> div > div:nth-child(2)
-{
     flex: 1;
-    display:flex;
-  
-    flex-wrap:wrap;
-    word-break:break;
-   
+    display: flex;
+    font-family: Montserrat;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 24px;
+    letter-spacing: 0em;
+    text-align: left;
+    align-items: center;
+    padding-left: 18px;
+    color: #233584;
+    border-radius: 12px;
+    word-break: break;
+    min-height: auto;
+    height: 60px;
 
-}
-* .choiceImage{
-    padding:0;
-}
-`
+    gap: 2rem;
+
+    border: 1px solid black;
+
+    height: auto;
+  }
+  > div > div {
+    min-width: auto;
+    min-height: auto;
+  }
+  > div > div:nth-child(2) {
+    flex: 1;
+    display: flex;
+
+    flex-wrap: wrap;
+    word-break: break;
+  }
+  * .choiceImage {
+    padding: 0;
+  }
+`;
